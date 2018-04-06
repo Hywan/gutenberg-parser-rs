c32103d1b807cbc5eda6830b7ad7b41414963a49
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2018-04-06 11:44:49 +0200

    re-enable wasm bindgen (can `build`, not `test`).

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 // Increase the macro recursion limit.
 #![recursion_limit="128"]
 
-//#![feature(proc_macro, wasm_custom_section, wasm_import_module)]
-//extern crate wasm_bindgen;
-//use wasm_bindgen::prelude::*;
+#![feature(proc_macro, wasm_custom_section, wasm_import_module)]
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
 
 #[macro_use]
 extern crate nom;
@@ -11,7 +11,7 @@ extern crate nom;
 use nom::IResult;
 
 /// Test
-//#[wasm_bindgen]
+#[wasm_bindgen]
 pub fn root(input: &str) -> bool {
     if let Ok(_) = block_list(input.as_bytes()) {
         true
