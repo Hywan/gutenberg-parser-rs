18fae149e23b5e5254905bf4828531da90f0a369
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2018-04-06 14:23:57 +0200

    fix: build script to use the release wasm build.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -ex
 
 cargo +nightly build --release --target wasm32-unknown-unknown
-wasm-bindgen target/wasm32-unknown-unknown/debug/parser.wasm --out-dir .
+wasm-bindgen target/wasm32-unknown-unknown/release/parser.wasm --out-dir .
 wasm-gc parser_bg.wasm
 wasm-opt -Oz -o parser_bg_opt.wasm parser_bg.wasm
 mv parser_bg_opt.wasm parser_bg.wasm
