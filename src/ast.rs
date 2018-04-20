456c13d606238a735427625cc8a55754cb5d1111
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2018-04-20 08:36:55 +0100

    feat(wasm) Don't derive `Debug`.
    It reduces the WASM file size by 2Kb (uncompressed), 1Kb (compressed).

--- src/ast.rs
+++ src/ast.rs
@@ -1,7 +1,8 @@
 use super::Input;
 #[cfg(feature = "wasm")] use alloc::Vec;
 
-#[derive(Debug, PartialEq)]
+#[derive(PartialEq)]
+#[cfg_attr(not(feature = "wasm"), Debug)]
 pub struct Block<'a> {
     pub name: (Input<'a>, Input<'a>),
     pub attributes: Option<Input<'a>>,
