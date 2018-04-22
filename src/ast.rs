c251f11b952b13a644dfa368c1173346d1432901
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2018-04-22 10:16:50 +0100

    fix(ast) Fix the `Debug` conditional feature.

--- src/ast.rs
+++ src/ast.rs
@@ -2,7 +2,7 @@ use super::Input;
 #[cfg(feature = "wasm")] use alloc::Vec;
 
 #[derive(PartialEq)]
-#[cfg_attr(not(feature = "wasm"), Debug)]
+#[cfg_attr(not(feature = "wasm"), derive(Debug))]
 pub struct Block<'a> {
     pub name: (Input<'a>, Input<'a>),
     pub attributes: Option<Input<'a>>,
