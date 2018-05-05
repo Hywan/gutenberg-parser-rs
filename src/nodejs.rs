a75ae1ad934c29181cab774e2cc6123430aa6f73
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2018-05-05 09:18:38 +0200

    feat(nodejs) Make it compile with the new AST.

--- src/nodejs.rs
+++ src/nodejs.rs
@@ -4,7 +4,7 @@ NodeJS bindings.
 
 */
 
-use super::ast::Block;
+use super::ast::Node;
 use neon::js::{JsArray, JsString, JsObject, Object};
 use neon::mem::Handle;
 use neon::scope::Scope;
@@ -44,53 +44,60 @@ register_module!(
     }
 );
 
-impl<'a> Block<'a> {
+impl<'a> Node<'a> {
     fn into_js_object<'b, S: Scope<'b>>(&self, scope: &mut S) -> JsResult<'b, JsObject> {
         let output = JsObject::new(scope);
 
-        output.set(
-            "name",
-            JsString::new_or_throw(
-                scope,
-                &format!(
-                    "{}/{}",
-                    unsafe { str::from_utf8_unchecked(self.name.0) },
-                    unsafe { str::from_utf8_unchecked(self.name.1) }
-                )
-            )?
-        )?;
-        output.set(
-            "attributes",
-            if let Some(attributes) = self.attributes {
-                JsString::new_or_throw(
-                    scope,
-                    unsafe { str::from_utf8_unchecked(attributes) }
-                )?
-            } else {
-                JsString::new_or_throw(
-                    scope,
-                    "{}"
-                )?
-            }
-        )?;
-
-        let mut inner_blocks = JsArray::new(scope, self.inner_blocks.len() as u32);
-
-        {
-            let raw_inner_blocks = inner_blocks.deref_mut();
-
-            for (index, block) in self.inner_blocks.iter().enumerate() {
-                raw_inner_blocks.set(
-                    index as u32,
-                    block.into_js_object(scope)?
+        match self {
+            Node::Block { name, attributes, children } => {
+                output.set(
+                    "name",
+                    JsString::new_or_throw(
+                        scope,
+                        &format!(
+                            "{}/{}",
+                            unsafe { str::from_utf8_unchecked(name.0) },
+                            unsafe { str::from_utf8_unchecked(name.1) }
+                        )
+                    )?
                 )?;
+                output.set(
+                    "attributes",
+                    if let Some(attributes) = attributes {
+                        JsString::new_or_throw(
+                            scope,
+                            unsafe { str::from_utf8_unchecked(attributes) }
+                        )?
+                    } else {
+                        JsString::new_or_throw(
+                            scope,
+                            "{}"
+                        )?
+                    }
+                )?;
+
+                let mut children_ = JsArray::new(scope, children.len() as u32);
+
+                {
+                    let raw_children = children_.deref_mut();
+
+                    for (index, block) in children.iter().enumerate() {
+                        raw_children.set(
+                            index as u32,
+                            block.into_js_object(scope)?
+                        )?;
+                    }
+                }
+
+                output.set(
+                    "children",
+                    children_
+                )?;
+            },
+
+            Node::Phrase(phrase) => {
             }
         }
-
-        output.set(
-            "inner_blocks",
-            inner_blocks
-        )?;
 
         Ok(output)
     }
