28b303235ba38b8129e52f173a1a253e60ff824c
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2018-05-09 11:33:35 +0200

    feat(bin) Bin version is defined by the project version.

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ macro_rules! to_str (
  
 fn main() { 
     let matches = App::new("gutenberg-post-parser")
-       .version("0.6.0")
+       .version(env!("CARGO_PKG_VERSION"))
        .about("Parse Gutenberg posts!")
        .author("Ivan Enderlin")
         .arg(
