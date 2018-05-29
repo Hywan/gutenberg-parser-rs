c37dbf3d06a5f80c97b2e99e0a4ca0459bc855b8
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2018-05-29 10:24:59 +0200

    feat(c) Continue the C bindings.

--- src/c.rs
+++ src/c.rs
@@ -4,17 +4,35 @@ C bindings.
 
 */
 
-use nom;
-use super::Input;
 use super::root;
-use super::ast;
+use std::ffi::CStr;
+use std::os::raw::c_char;
 
+/// cbindgen:derive-helper-methods
 #[repr(C)]
-pub struct Foo {
-    bar: u8
+pub enum Result {
+    Ok(Vec<Node>),
+    Err
+}
+
+/// cbindgen:derive-helper-methods
+#[repr(C)]
+pub enum Node {
+    Block {
+        name: *const c_char,
+        attributes: Option<*const c_char>,
+        children: Vec<Node>
+    },
+    Phrase(*const c_char)
 }
 
 #[no_mangle]
-pub extern "C" fn parse(input: *mut u8) -> Foo {
-    Foo { bar: b' ' }
+pub extern "C" fn parse(pointer: *const c_char) -> Result {
+    let input = unsafe { CStr::from_ptr(pointer).to_bytes() };
+
+    if let Ok((_remaining, _nodes)) = root(input) {
+        Result::Ok(vec![Node::Phrase(b' ' as *const c_char)])
+    } else {
+        Result::Err
+    }
 }
