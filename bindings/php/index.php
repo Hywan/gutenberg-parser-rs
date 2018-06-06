08d1c567defadc3868a5be77ef7e742aa1e3b906
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2018-06-06 17:23:04 +0200

    feat(php) Add shebang to load the extension and run the program directly.

--- bindings/php/index.php
+++ bindings/php/index.php
@@ -1,4 +1,9 @@
+#!/usr/bin/env php -dextension=gutenberg_post_parser
 <?php
+
+if (false === extension_loaded('gutenberg_post_parser')) {
+    die('The `gutenberg_post_parser` extension is not loaded.');
+}
 
 const POST = '<!-- wp:foo {"abc": "def"} /-->bar<!-- wp:b/az -->qux<!-- wp:hello /--><!-- /wp:b/az -->';
 
