57fd58d3f4a1dd53ba2a4a844290eed2dccbbdaf
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2018-06-28 13:56:35 +0200

    feat(wasm) Add a proper CLI for WASM!

--- bindings/wasm/bin/index.mjs
+++ bindings/wasm/bin/index.mjs
@@ -5,15 +5,15 @@ import util from 'util';
 import process from 'process';
 
 class Gutenberg_Post_Parser_NodeJS extends Gutenberg_Post_Parser {
-      constructor(Block, Phrase, wasmURL) {
-          super(Block, Phrase, wasmURL, new util.TextEncoder(), new util.TextDecoder());
-      }
+    constructor(Block, Phrase, wasmURL) {
+        super(Block, Phrase, wasmURL, new util.TextEncoder(), new util.TextDecoder());
+    }
 
-      instantiateWASM(url, importObject) {
-          const buffer = fs.readFileSync(url);
+    instantiateWASM(url, importObject) {
+        const buffer = fs.readFileSync(url);
 
-          return this._wasm = WebAssembly.instantiate(buffer, {}).then(obj => obj.instance);
-      }
+        return this._wasm = WebAssembly.instantiate(buffer, {}).then(obj => obj.instance);
+    }
 }
 
 class Block {
@@ -30,30 +30,95 @@ class Phrase {
     }
 }
 
-const parser = new Gutenberg_Post_Parser_NodeJS(Block, Phrase, './gutenberg_post_parser.wasm');
-
 const stdin = process.stdin;
 const stdout = process.stdout;
+const stderr = process.stderr;
+
+function usage() {
+    return 'USAGE:' + "\n" +
+        '    ' + process.argv0 + ' [FLAGS] [INPUT]' + "\n\n" +
+        'FLAGS:' + "\n" +
+        '    -d, --emit-debug    Compile the AST into JS debug format (default).' + "\n" +
+        '    -j, --emit-json     Compile the AST into JSON format.' + "\n" +
+        '    -h, --help          Prints help information.' + "\n\n" +
+        'ARGS:' + "\n" +
+        '    <INPUT>    File containing the input.' + "\n";
+}
+
+if (2 >= process.argv.length) {
+    stderr.write(usage());
+
+    process.exit(1);
+}
+
+const parser = new Gutenberg_Post_Parser_NodeJS(Block, Phrase, './gutenberg_post_parser.wasm');
+
 let input = '';
+let emit = 'debug';
 
-stdin.setEncoding('utf-8');
-stdin.on(
-    'readable',
-    () => {
-        let chunk;
+process.argv.slice(2).forEach(
+    (argument_value) => {
+        switch (argument_value) {
+            case '-h':
+            case '--help':
+                stderr.write(usage());
 
-        while (chunk = stdin.read()) {
-            input += chunk;
+                process.exit(2);
+
+            case '-d':
+            case '--emit-debug':
+                emit = 'debug';
+
+                break;
+
+            case '-j':
+            case '--emit-json':
+                emit = 'json';
+
+                break;
+
+            default:
+                if (argument_value && '-' === argument_value[0]) {
+                    stderr.write('Argument `' + argument_value + '` is invalid.' + "\n\n");
+                    stderr.write(usage());
+
+                    process.exit(3);
+                }
+
+                input = argument_value;
         }
     }
 );
-stdin.on(
-    'end',
-    () => {
-        parser.root(input).then(
-            (output) => {
-                stdout.write(JSON.stringify(output, null, 2));
-            }
-        );
+
+if (!input) {
+    stderr.write('File is missing.' + "\n\n");
+    stderr.write(usage());
+
+    process.exit(4);
+}
+
+try {
+    fs.accessSync(input, fs.constants.R_OK)
+} catch (e) {
+    stderr.write('File `' + input + '` does not exist, or is not readable.' + "\n");
+
+    process.exit(5);
+}
+
+const content = fs.readFileSync(input, { encoding: 'utf-8' });
+
+parser.root(content).then(
+    (output) => {
+        switch (emit) {
+            case 'debug':
+                console.log(output);
+
+                break;
+
+            case 'json':
+                stdout.write(JSON.stringify(output, null, 4));
+
+                break;
+        }
     }
 );
